7d02394a2fd59d3fd3b1eeaa6a418f31c588cb3f
Bianca <biancappv@gmail.com>
2024-04-15 20:12:28 -0300

    blog_pessoal

---
+++ src/postagem/entities/postagem.entity.ts
@@ -0,0 +1,21 @@
+import { IsNotEmpty } from "class-validator";
+import { Column, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity({name: "tb_postagens" })
+export class Postagem {
+
+    @PrimaryGeneratedColumn() // define a chave primária e auto_increment
+    id: number;
+
+    @IsNotEmpty()
+    @Column({length: 100, nullable: false  })
+    titulo: string;
+
+   @IsNotEmpty()
+    @Column({length: 1000, nullable: false  })
+    texto: string;
+
+    @UpdateDateColumn()
+    date: Date
+
+}
